0bf38412a59be1bc142c2fa5288be30ff61886b1
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-06-19 22:52:19 +0900

    ビューの調整と場所の入力フォームの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_084633) do
+ActiveRecord::Schema.define(version: 2020_06_19_125245) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "event_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_084633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "place"
   end
 
 end
